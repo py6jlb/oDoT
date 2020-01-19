c4e5de6cf7ef26e6661b554bbb370e122b07b6bd
Апогеев Митрофан <py6jlb@gmail.com>
2020-01-19 16:07:23 +0300

    [изм] -поправил сохранение настроек

--- BusinessLogic/Abstractions/ISettingsService.cs
+++ BusinessLogic/Abstractions/ISettingsService.cs
@@ -4,6 +4,7 @@ using BusinessLogic.DTO;
 namespace BusinessLogic.Abstraction{
     public interface ISettingsService{
         SettingsDto GetSettings();
+        SettingsDto AddSettings(SettingsDto newData);
         SettingsDto SaveSettings(SettingsDto newData);
     }
 }
